d55d682cb49fc2c074a843fcdf5d63af8578e552
DeltaProxima <vadeshpande2002@gmail.com>
2024-10-03 15:50:00 +0530

    Added code for interation with GPT

--- agent.py
+++ agent.py
@@ -1,5 +1,8 @@
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
+import os
+from  dotenv import load_dotenv
 
-# model = ChatOpenAI(model="gpt-4o")
+load_dotenv()
+model = ChatOpenAI(model="gpt-4o", openai_api_key=os.getenv("OPENAI_API_KEY"))
 
--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from fastapi import FastAPI, File, UploadFile, HTTPException,Depends,Form
 from typing import List, Optional
 import uuid
 import aiofiles
-from agent import *
+from agent import model
 from prompt import road_walkability_prompt
+from langchain_core.messages import HumanMessage
 from sqlalchemy.orm import sessionmaker,Session
 from database import Base, SessionLocal
 from models import RoadImages
 import sqlite3
-import base64
 import httpx
+import base64
 import os
 
 
@@ -28,12 +29,15 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 @app.post("/upload-image/")
-async def upload_image(lat: str=Form(...), long: str=Form(...), file: UploadFile = File(...), db: Session = Depends(get_db)):
+async def upload_image( file: UploadFile ,lat: str=Form(...), long: str=Form(...), db: Session = Depends(get_db)):
     # Generate a unique filename
     file_extension = file.filename.split(".")[-1]
     file.filename = str(uuid.uuid4())+"." + file_extension
     file_location = f"{UPLOAD_FOLDER}/{file.filename}"
-    
+
+    # extract content out of the file
+    data = await file.read()
+    image_data = base64.b64encode(data).decode("utf-8")    
     # Save the uploaded file
     async with aiofiles.open(file_location, 'wb') as out_file:
         content = await file.read()
@@ -44,6 +48,18 @@ async def upload_image(lat: str=Form(...), long: str=Form(...), file: UploadFile
     db.add(image)
     db.commit()
     db.refresh(image)
+
+    # Asking GPT to desribe the image
+    message = HumanMessage(
+        content=[
+            {"type": "text", "text": "describe the road quality in this image"},
+            {
+                "type": "image_url",
+                "image_url": {"url": f"data:image/jpeg;base64,{image_data}"},
+            },
+        ],
+    )
+    response = model.invoke([message])
     
-    return {"filename": file.filename, "latitude": lat, "longitude": long}
+    return {"filename": file.filename, "latitude": lat, "longitude": long, "response": response.content}
 

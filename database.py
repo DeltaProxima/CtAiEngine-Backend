2c0ba22661c5150e5fc3ea13fbce2fa90745e67a
Arpit Sureka <surekaarpit@gmail.com>
2024-10-15 00:32:03 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,10 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.declarative import DeclarativeMeta
 import json
+import os
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./roadImages.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(os.getenv("SQLALCHEMY_DATABASE_URL"), connect_args={'check_same_thread': False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
